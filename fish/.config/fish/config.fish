87dc0fef04b682b009caad99f8abb9acb9d316b3
Travis A. Dula <travisadula@gmail.com>
2021-03-27 12:53:15 -0500

    fish: skim keybinds, pmix abbr, cmd not found

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,6 @@
 fish_vi_key_bindings
+skim_key_bindings
 set SEM $HOME/docs/school/2021_spring/
+function __fish_command_not_found_handler --on-event fish_command_not_found
+    echo "fish: Unknown command '$argv'"
+end

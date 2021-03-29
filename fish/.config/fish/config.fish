e9f298c5af3f078a874f4b9f48f032feef13a824
Travis A. Dula <travisadula@gmail.com>
2021-03-29 18:38:06 -0500

    changes bat theme, alias lf=lfcd

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 fish_vi_key_bindings
 skim_key_bindings
 set SEM $HOME/docs/school/2021_spring/
+set -x BAT_THEME Nord
 function __fish_command_not_found_handler --on-event fish_command_not_found
     echo "fish: Unknown command '$argv'"
 end

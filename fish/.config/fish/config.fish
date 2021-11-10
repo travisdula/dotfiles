5c1504032040c7d97b82797b85ae6e7b1b1edfde
Travis A. Dula <travisadula@gmail.com>
2021-11-09 18:22:53 -0600

    changed semester

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 fish_vi_key_bindings
 skim_key_bindings
-set SEM $HOME/docs/school/2021_spring/
+set SEM $HOME/docs/school/2021_fall/
 set -x BAT_THEME Nord
 function __fish_command_not_found_handler --on-event fish_command_not_found
     if test -d $argv

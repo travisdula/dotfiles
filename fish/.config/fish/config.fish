860977d265a36426b77fc6a9b1fa8b8c1e063ff6
Travis A. Dula <travisadula@gmail.com>
2022-01-12 16:19:01 -0600

    changed semester, added startx and note generator on login

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 fish_vi_key_bindings
 skim_key_bindings
-set SEM $HOME/docs/school/2021_fall/
+set SEM $HOME/docs/school/2022_spring/
 set -x BAT_THEME Nord
 function __fish_command_not_found_handler --on-event fish_command_not_found
     if test -d $argv
@@ -8,4 +8,9 @@ function __fish_command_not_found_handler --on-event fish_command_not_found
     else
         echo "fish: Unknown command '$argv'"
     end
+end
+
+if status --is-login
+    notes-generator &
+    startx
 end

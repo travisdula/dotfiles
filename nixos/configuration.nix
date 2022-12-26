0bb469f2bd6bc8142b840eb0b1e5bab80b3baf85
Travis A. Dula <travisadula@gmail.com>
2022-12-25 23:00:21 -0600

    change nixos config from link to file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,1 +1,191 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot/efi";
+  };
+
+  networking = {
+    hostName = "envy"; # Define your hostname.
+    networkmanager.enable = true;
+    # Open ports in the firewall.
+    # firewall.allowedTCPPorts = [ ... ];
+    # firewall.allowedUDPPorts = [ ... ];
+    # Or disable the firewall altogether.
+    # firewall.enable = false;
+    # working.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  };
+
+
+  # Set your time zone.
+  time.timeZone = "America/Chicago";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services = {
+    xserver = {
+      layout = "us";
+      xkbVariant = "";
+      enable = true;
+      windowManager.qtile.enable = true;
+      displayManager.lightdm.enable = true;
+      libinput = {
+        enable = true;
+        mouse.accelProfile = "flat";
+        touchpad = {
+          accelProfile = "adaptive"; 
+          disableWhileTyping = true;
+        };
+      };
+    };
+    picom.enable = true;
+    redshift = {
+      enable = true;
+      brightness = {
+        day = "1";
+	night = "1";
+      };
+      temperature = {
+        day = 5500;
+	night = 3700;
+      };
+    };
+    unclutter.enable = true;
+    blueman.enable = true;
+  };
+
+  sound.enable = true;
+  hardware = {
+    pulseaudio.enable = true;
+    bluetooth.enable = true;
+  };
+
+  location.provider = "geoclue2";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.travis = {
+    shell = pkgs.fish;
+    isNormalUser = true;
+    description = "Travis A. Dula";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
+  };
+
+  programs = {
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+    fish.enable = true;
+  };
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    jetbrains-mono
+    nerdfonts
+  ];
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # tui
+    neovim
+    bat
+    lf
+    pulsemixer
+    htop
+    gotop
+    # cli
+    exa
+    fd
+    ripgrep
+    wget
+    python
+    pandoc
+    gcc
+    git
+    brightnessctl
+    xclip # needed for nvim unnamedplus clipboard
+    stow
+    # gui
+    sxiv
+    mpv
+    zathura
+    gimp
+    firefox
+    flameshot
+    qtile
+    kitty
+    discord
+    firefox
+    spotify
+    pavucontrol
+    dmenu
+    libreoffice
+  ];
+  nixpkgs.overlays = [(self: super: { discord = super.discord.overrideAttrs (_: { src = builtins.fetchTarball "https://discord.com/api/download?platform=linux&format=tar.gz"; });})];
+
+  #xdg.mime.defaultApplications = {
+  #  application/pdf = "zathura.desktop";
+  #  image/png = "sxiv.desktop";
+  #};
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.11"; # Did you read the comment?
+}

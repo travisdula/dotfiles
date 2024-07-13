4d230abb56bc9826f3d1519edace1fd7c29b2b81
Travis A. Dula <travisadula@gmail.com>
2024-07-13 14:07:47 -0500

    removed xdg dir comments

--- home-manager/programs/fish.nix
+++ home-manager/programs/fish.nix
@@ -13,16 +13,6 @@
     #set TEXMFHOME $XDG_DATA_HOME/texmf
     #set TEXMFVAR $XDG_CACHE_HOME/texlive/texmf-va
     #set TEXMFCONFIG $XDG_CONFIG_HOME/texlive/texmf-config
-    #
-    ## my standard home directories
-    #set XDG_DESKTOP_DIR "$HOME/desk"
-    #set XDG_DOCUMENTS_DIR "$HOME/docs"
-    #set XDG_DOWNLOAD_DIR "$HOME/dl"
-    #set XDG_MUSIC_DIR "$HOME/mus"
-    #set XDG_PICTURES_DIR "$HOME/pics"
-    #set XDG_PUBLICSHARE_DIR "$HOME/pub"
-    #set XDG_TEMPLATES_DIR "$HOME/templ"
-    #set XDG_VIDEOS_DIR "$HOME/vids"
 
     enable = true;
     shellAbbrs = {

806c8ccbab1d1ffd024ba46702d3fd4025f66b1f
Travis A. Dula <travisadula@gmail.com>
2026-06-07 15:16:04 -0500

    chore: missed one ordering

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -91,7 +91,6 @@
     backupFileExtension = ".bak";
   };
 
-  location.provider = "geoclue2";
   i18n = {
     defaultLocale = "en_US.UTF-8";
     extraLocaleSettings = {
@@ -106,6 +105,8 @@
       LC_TIME = "en_US.UTF-8";
     };
   };
+
+  location.provider = "geoclue2";
 
   networking = {
     hostName = "thethinker";
@@ -192,13 +193,7 @@
     blueman.enable = true;
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Only update on new installs
 
   time.timeZone = "America/Chicago";
 

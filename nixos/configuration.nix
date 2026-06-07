e153584ca26b0ea72db8b636daf47d752077fb6b
Travis A. Dula <travisadula@gmail.com>
2026-06-07 15:11:03 -0500

    chore: clean up configuration.nix and alphabetize top level

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -1,5 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
+# Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { pkgs, ... }:
@@ -7,7 +6,7 @@
   imports = [
     <home-manager/nixos>
     <nixos-hardware/lenovo/thinkpad/x1/6th-gen>
-    ./hardware-configuration.nix # Include the results of the hardware scan.
+    ./hardware-configuration.nix
   ];
 
   boot.loader = {
@@ -17,163 +16,15 @@
       efiSupport = true;
       useOSProber = true;
     };
-    #systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-    efi.efiSysMountPoint = "/boot/efi";
-  };
-
-  networking = {
-    hostName = "thethinker"; # Define your hostname.
-    networkmanager.enable = true;
-    # Open ports in the firewall.
-    # firewall.allowedTCPPorts = [ ... ];
-    # firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # firewall.enable = false;
-    # working.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-    # Configure network proxy if necessary
-    # proxy.default = "http://user:password@proxy:port/";
-    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  };
-
-  # Set your time zone.
-  time.timeZone = "America/Chicago";
-
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
-  #virtualisation = {
-  #  docker = {
-  #    enable = true;
-  #    rootless = {
-  #      enable = true;
-  #      setSocketVariable = true;
-  #    };
-  #  };
-  #};
-
-  services = {
-    ntp.enable = true;
-    fprintd = {
-      enable = true;
-      tod = {
-        enable = true;
-        driver = pkgs.libfprint-2-tod1-goodix;
-      };
-    };
-
-    pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-      #extraConfig.pipewire."92-low-latency";
-    };
-
-    xserver = {
-      xkb.layout = "us";
-      xkb.variant = "";
-      enable = true;
-      windowManager.qtile = {
-        enable = true;
-        #backend = "wayland"; # TODO wayland
-      };
-      displayManager.lightdm = {
-        extraConfig = "user-authority-in-system-dir = true\n";
-        enable = true;
-      };
-    };
-
-    libinput = {
-      enable = true;
-      mouse.accelProfile = "flat";
-      touchpad = {
-        accelProfile = "adaptive";
-        disableWhileTyping = true;
-      };
-    };
-
-    picom.enable = true;
-
-    redshift = {
-      enable = true;
-      brightness = {
-        day = "1";
-        night = "0.8";
-      };
-      temperature = {
-        day = 5500;
-        night = 3700;
-      };
-    };
-
-    unclutter-xfixes = {
-      enable = true;
-      extraOptions = [ "noevents" ];
-    };
-
-    blueman.enable = true;
-  };
-
-  hardware = {
-    bluetooth = {
-      enable = true;
-      powerOnBoot = true;
-    };
-    trackpoint = {
-      enable = true;
-      speed = 255;
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
     };
   };
-
-  location.provider = "geoclue2";
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.travis = {
-    shell = pkgs.fish;
-    isNormalUser = true;
-    description = "Travis A. Dula";
-    extraGroups = [
-      "networkmanager"
-      "wheel"
-    ];
-  };
-
-  home-manager = {
-    users.travis = import /home/travis/dotfiles/home-manager/home.nix;
-    backupFileExtension = ".bak";
-  };
-  programs = {
-    fish.enable = true;
-  };
-
-  fonts.packages = with pkgs; [
-    noto-fonts
-    noto-fonts-cjk-sans
-    noto-fonts-color-emoji
-    jetbrains-mono
-    nerd-fonts.jetbrains-mono
-  ];
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
 
   environment = {
     sessionVariables = {
-      GTK_THEME = "Nordic"; # Adwaita:dark if don't want Nord
+      GTK_THEME = "Nordic";
     };
 
     systemPackages = with pkgs; [
@@ -216,18 +67,50 @@
     ];
   };
 
-  xdg.mime.defaultApplications = {
-    "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
-    "image/*" = "sxiv.desktop";
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-color-emoji
+    jetbrains-mono
+    nerd-fonts.jetbrains-mono
+  ];
+
+  hardware = {
+    bluetooth = {
+      enable = true;
+      powerOnBoot = true;
+    };
+    trackpoint = {
+      enable = true;
+      speed = 255;
+    };
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  home-manager = {
+    users.travis = import /home/travis/dotfiles/home-manager/home.nix;
+    backupFileExtension = ".bak";
+  };
+
+  location.provider = "geoclue2";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
+  };
+
+  networking = {
+    hostName = "thethinker";
+    networkmanager.enable = true;
+  };
 
   nix = {
     gc = {
@@ -242,5 +125,95 @@
         "flakes"
       ];
     };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.fish.enable = true;
+
+  services = {
+    ntp.enable = true;
+    fprintd = {
+      enable = true;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-goodix;
+      };
+    };
+
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+    };
+
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+      windowManager.qtile.enable = true;
+      displayManager.lightdm = {
+        extraConfig = "user-authority-in-system-dir = true\n";
+        enable = true;
+      };
+    };
+
+    libinput = {
+      enable = true;
+      mouse.accelProfile = "flat";
+      touchpad = {
+        accelProfile = "adaptive";
+        disableWhileTyping = true;
+      };
+    };
+
+    picom.enable = true;
+
+    redshift = {
+      enable = true;
+      brightness = {
+        day = "1";
+        night = "0.8";
+      };
+      temperature = {
+        day = 5500;
+        night = 3700;
+      };
+    };
+
+    unclutter-xfixes = {
+      enable = true;
+      extraOptions = [ "noevents" ];
+    };
+
+    blueman.enable = true;
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+  time.timeZone = "America/Chicago";
+
+  users.users.travis = {
+    shell = pkgs.fish;
+    isNormalUser = true;
+    description = "Travis A. Dula";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+    ];
+  };
+
+  xdg.mime.defaultApplications = {
+    "application/pdf" = "org.pwmt.zathura-pdf-mupdf.desktop";
+    "image/*" = "sxiv.desktop";
   };
 }

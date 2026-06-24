7364d7d6f71bea7fb5213c6b7f9816be78cb8c81
Travis A. Dula <travisadula@gmail.com>
2026-06-23 20:03:32 -0500

    chore: don't pcall since I am on Nix

--- home-manager/programs/nvim/init.lua
+++ home-manager/programs/nvim/init.lua
@@ -71,14 +71,12 @@ vim.lsp.config('*', {
 vim.lsp.enable({ 'lua_ls', 'nixd' })
 
 -- TELESCOPE
-local has_telescope, builtin = pcall(require, 'telescope.builtin')
-if has_telescope then
-    vim.keymap.set('n', '<leader>fF', builtin.find_files, { desc = 'Find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
-    vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Find git files' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
-    vim.keymap.set('n', '<leader>fs', function()
-        builtin.lsp_document_symbols({ ignore_symbols = 'string' })
-    end, { desc = 'LSP Document symbols' })
-end
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>fF', builtin.find_files, { desc = 'Find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Live grep' })
+vim.keymap.set('n', '<leader>ff', builtin.git_files, { desc = 'Find git files' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Find buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Help tags' })
+vim.keymap.set('n', '<leader>fs', function()
+    builtin.lsp_document_symbols({ ignore_symbols = 'string' })
+end, { desc = 'LSP Document symbols' })
